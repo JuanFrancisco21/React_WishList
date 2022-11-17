2c94ce77320decd72113837885df9e4482d72e95
JuanFrancisco21 <juanfranciscoaguilarsanchez@gmail.com>
2022-11-17 18:43:32 +0100

    Nuevo diseño y boton para la creacion de wishes

--- src/components/Wishinput.jsx
+++ src/components/Wishinput.jsx
@@ -7,18 +7,38 @@ function WishInput({ onNewWish }) {
   return (
     <fieldset className="form-group">
       <legend>New Wish</legend>
-      <input
-        className="form-control"
-        type="text"
-        placeholder="Introduce tu deseo"
-        ref={inputText}
-        onKeyUp={(event) => {
-          if (event.key === 'Enter' && event.target.value.length > 0) {
-            onNewWish({ id: Uuidv4(), text: event.target.value, done: false });
-            inputText.current.value = '';
-          }
-        }}
-      />
+      <div className="search-bar">
+        <div className='search'>
+        <input
+          className="input"
+          type="text"
+          placeholder="Introduce tu deseo"
+          ref={inputText}
+          onKeyUp={(event) => {
+            if (event.key === 'Enter' && event.target.value.length > 0) {
+              onNewWish({ id: Uuidv4(), text: event.target.value, done: false });
+              inputText.current.value = '';
+            }
+          }}
+        />
+        </div>
+        <div className="create">
+        <button className="create-btn">
+          <div className="svg-wrapper-1">
+            <div className="svg-wrapper">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+                <path fill="none" d="M0 0h24v24H0z"></path>
+                <path fill="currentColor" d="M1.946 9.315c-.522-.174-.527-.455.01-.634l19.087-6.362c.529-.176.832.12.684.638l-5.454 19.086c-.15.529-.455.547-.679.045L12 14l6-8-8 6-8.054-2.685z"></path>
+              </svg>
+            </div>
+          </div>
+          <span className='span'>Send</span>
+        </button>
+        </div>
+
+       
+        
+      </div>
     </fieldset>
   );
 }
@@ -28,7 +48,7 @@ WishInput.propTypes = {
 };
 
 WishInput.defaultProps = {
-  onNewWish: () => {},
+  onNewWish: () => { },
 };
 
 export default WishInput;
